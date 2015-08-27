51c2b7d163ed5e7099ef948231c5236a55b894f9
Tyler-Yates <tyler.yates@utexas.edu>
2015-08-27 16:14:33 -0500

    Add tests for Word class.

--- src/test/java/com/tyleryates/util/WordTest.java
+++ src/test/java/com/tyleryates/util/WordTest.java
@@ -1,18 +1,37 @@
 package com.tyleryates.util;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @SuppressWarnings("JavaDoc")
 public class WordTest {
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
     private Word word;
+    private static final String STRING = "lottery";
 
     @Before
     public void setUp() throws Exception {
-        word = new Word("lottery");
+        word = new Word(STRING);
+    }
+
+    @Test
+    public void testWordConstructor() {
+        exception.expect(IllegalArgumentException.class);
+        new Word(null);
+    }
+
+    @Test
+    public void testGetWord() {
+        assertEquals(STRING, word.getString());
+        assertEquals("", new Word("").getString());
     }
 
     @Test
