a7bd77f34713e91cc7b87c6f93d4b36e551785b2
Tyler-Yates <tyler.yates@utexas.edu>
2015-08-28 21:04:30 -0500

    Add tests for randomInt methods of Integers class.

--- src/test/java/com/tyleryates/util/IntegersTest.java
+++ src/test/java/com/tyleryates/util/IntegersTest.java
@@ -1,15 +1,45 @@
 package com.tyleryates.util;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Tests {@link Integers}.
  */
 @SuppressWarnings("JavaDoc")
 public class IntegersTest {
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    private Random MAX_RANDOM;
+    private Random MIN_RANDOM;
+
+    @Before
+    public void setup() {
+        MAX_RANDOM = mock(Random.class);
+        when(MAX_RANDOM.nextInt(anyInt())).thenAnswer(new Answer<Integer>() {
+            @Override
+            public Integer answer(InvocationOnMock invocationOnMock) throws Throwable {
+                return (int) invocationOnMock.getArguments()[0] - 1;
+            }
+        });
+
+        MIN_RANDOM = mock(Random.class);
+        when(MIN_RANDOM.nextInt(anyInt())).thenReturn(0);
+    }
 
     @Test
     public void testIsPositive() {
@@ -73,5 +103,32 @@ public class IntegersTest {
         assertTrue(Integers.isOdd(1));
         assertTrue(Integers.isOdd(-1));
         assertTrue(Integers.isOdd(-2_000_000_001));
+    }
+
+    @Test
+    public void testRandomIntIllegalBound() {
+        exception.expect(IllegalArgumentException.class);
+        Integers.randomInt(1, 0);
+
+        exception.expect(IllegalArgumentException.class);
+        Integers.randomInt(1, -1);
+
+        exception.expect(IllegalArgumentException.class);
+        Integers.randomInt(1, 1);
+    }
+
+    @Test
+    public void testRandomIntNullRandom() {
+        exception.expect(IllegalArgumentException.class);
+        Integers.randomInt(0, 1, null);
+    }
+
+    @Test
+    public void testRandomInt() {
+        assertEquals(5, Integers.randomInt(0, 5, MAX_RANDOM));
+        assertEquals(Integer.MAX_VALUE, Integers.randomInt(Integer.MIN_VALUE, Integer.MAX_VALUE, MAX_RANDOM));
+
+        assertEquals(0, Integers.randomInt(0, Integer.MAX_VALUE, MIN_RANDOM));
+        assertEquals(Integer.MIN_VALUE, Integers.randomInt(Integer.MIN_VALUE, Integer.MAX_VALUE, MIN_RANDOM));
     }
 }
